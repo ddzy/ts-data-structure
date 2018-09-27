ebfc5d6bc2e94ec9a9d97e50598442470929fbec
yang <1766083035@qq.com>
2018-09-27 15:18:38 +0800

    feat * complete the bubbleSort method

--- build/07-排序.js
+++ build/07-排序.js
@@ -15,5 +15,35 @@ var ArrayList = /** @class */ (function () {
             ? ''
             : this.arr.join();
     };
+    ArrayList.prototype._swap = function (current, next) {
+        var temp = this.arr[current];
+        this.arr[current] = this.arr[next];
+        this.arr[next] = temp;
+    };
+    /**
+     * 冒泡排序
+     * 特点: 比较任何两个相邻的项, 如果第一个比第二个大, 则交换他们
+     */
+    ArrayList.prototype.bubbleSort = function () {
+        var arr = this.arr;
+        var len = arr.length;
+        for (var i = 0; i < len; i++) {
+            for (var j = 0; j < len - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    this._swap(j, j + 1);
+                }
+            }
+        }
+    };
     return ArrayList;
 }());
+var sort = new ArrayList();
+sort
+    .insert(20)
+    .insert(55)
+    .insert(10)
+    .insert(98)
+    .insert(67)
+    .insert(34);
+sort.bubbleSort();
+console.log(sort.print());
--- src/07-排序.ts
+++ src/07-排序.ts
@@ -2,14 +2,15 @@
  * 排序算法
  */
 class ArrayList {
-  private readonly arr: any[] = []
+
+  private readonly arr: number[] = []
 
   public constructor() {}
 
   public insert(
-    item: any,
+    item: number,
   ): ArrayList {
-    this.arr.push(item); 
+    this.arr.push(item);
 
     return this;
   }
@@ -19,5 +20,51 @@ class ArrayList {
       ? ''
       : this.arr.join();
   }
+
+  protected _swap(
+    current: number, 
+    next: number,
+  ) {
+    let temp: number = this.arr[current];
+    this.arr[current] = this.arr[next]; 
+    this.arr[next] = temp;
+  }
+
+
+  /**
+   * 冒泡排序
+   * 特点: 比较任何两个相邻的项, 如果第一个比第二个大, 则交换他们
+   */
+  public bubbleSort(): void {
+    const arr: number[] = this.arr;
+    const len: number = arr.length;
+
+    for(let i = 0; i < len; i++) {
+      for(let j = 0; j < len - 1; j++) {
+        if(arr[j] > arr[j+1]) {
+          this._swap(j, j+1);
+        }
+      }
+    }
+  }
 }
+
+
+const sort: ArrayList = new ArrayList();
+
+sort
+  .insert(20)
+  .insert(55)
+  .insert(10)
+  .insert(98)
+  .insert(67)
+  .insert(34)
+
+sort.bubbleSort();
+
+
+console.log(sort.print());
+
+
+
 
